f51947a0928183b03ca759bc3cda499f18f36f70
NathanJ4620 <nathanjseeley@gmail.com>
2014-10-27 22:24:36 -0600

    Added the .exe (I think) and poked at making a seperate class to handle calculations.

---
+++ Creepy Calc/Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creepy_Calc
+{
+    class Calc
+    {
+        public decimal oldest;
+        public decimal youngest;
+        public decimal minage;
+        public bool isCreepy;
+        public decimal ageGap;
+
+        
+    }
+}
